53c456b6a4e800164387a627f52c040b186b89d0
HSgod <hsgodkontakt@gmail.com>
2020-10-10 10:52:15 +0000

    Heavens: Settings: Reorganize configs
    Signed-off-by: HSgod <hsgodkontakt@gmail.com>
    Signed-off-by: Egii <regidesoftian@gmail.com>
    Change-Id: Ia0c401c131d9fcc12520f9a9a7a72ce70659ff69

--- src/com/heavens/settings/HeavensSettings.java
+++ src/com/heavens/settings/HeavensSettings.java
@@ -25,7 +25,6 @@ import android.os.Bundle;
 import android.view.Surface;
 import android.preference.Preference;
 import com.android.settings.R;
-import com.heavens.settings.preferences.Utils;
 
 import com.android.settings.SettingsPreferenceFragment;
 
@@ -34,15 +33,8 @@ public class HeavensSettings extends SettingsPreferenceFragment {
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-        final String KEY_DEVICE_PART = "device_part";
-        final String KEY_DEVICE_PART_PACKAGE_NAME = "org.omnirom.device";
 
         addPreferencesFromResource(R.xml.heavens_settings);
-
-        // DeviceParts
-        if (!Utils.isPackageInstalled(getActivity(), KEY_DEVICE_PART_PACKAGE_NAME)) {
-            getPreferenceScreen().removePreference(findPreference(KEY_DEVICE_PART));
-        }
 
     }
 
